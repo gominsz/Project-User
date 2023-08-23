26143648e80ca6a27a5f1f5922c53000cb50be62
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-23 19:18:55 -0300

    add-PrismaClient-module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserResolver } from './user.resolver';
+import { PrismaClient } from '@prisma/client';
+import { UserController } from './user.controller';
+
 
 @Module({
-  providers: [UserResolver, UserService]
+  providers: [UserService, PrismaClient],
+  controllers: [UserController],
+  
 })
 export class UserModule {}
