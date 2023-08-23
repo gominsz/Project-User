9c73e757f1c082c5d5428fe3aba47e4d4ef40c37
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-23 19:18:17 -0300

    Routes/users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserInput } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly useService: UserService) {}
+
+
   @Get()
-  findAll(): string {
-    return 'This action returns all user';
+  findAll() {
+    return this.useService.findAll();
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {   
+    return this.useService.create(createUserDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.useService.findOne(id);
+  }
+
+  @Patch(':id') 
+  update(
+    @Param('id') id: string, 
+    @Body()  updateUserInput:  UpdateUserInput) {
+    return this.useService.update(id,  updateUserInput);
+  }
+
+  @HttpCode(204) // representa No content (Sem conteudo)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.useService.remove(id);
   }
 }
