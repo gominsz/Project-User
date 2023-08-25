18f4281d39becc95402b411749637da5f990a60a
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-25 17:51:51 -0300

    class-validator

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsNotEmpty, IsString, MaxLength } from "class-validator";
+
+
 export class CreateUserDto {
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
   password: string;
 }
