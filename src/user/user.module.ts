c88fa18950e80ea6855053df27d76e108f1b5299
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-28 17:59:16 -0300

    exports -  [UserService] Use.module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PrismaClient } from '@prisma/client';
 import { UserController } from './user.controller';
-import { AuthModule } from 'src/auth/auth.module';
 
 
 @Module({
   providers: [UserService, PrismaClient],
   controllers: [UserController],
+  exports: [UserService] 
   
 })
 export class UserModule {}
