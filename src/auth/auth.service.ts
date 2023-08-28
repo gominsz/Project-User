422f1a007decc12de0705c297f9fae1ed39ef494
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-28 17:50:11 -0300

    UserPayload ( access_token: jwtToken) find isPasswaordValid/Error(email/passwoard) access_token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,47 @@
-import { Injectable, Dependencies, UnauthorizedException } from '@nestjs/common';
-import { UserService } from '../user/user.service';
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { UserPayload } from './models/UserPayload';
+import { User } from '../user/entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserTokens';
 
 @Injectable()
-@Dependencies(UserService)
 export class AuthService {
-  constructor(private userService, private jwtService: JwtService) {}
+    constructor(private readonly userService: UserService, private readonly jwtService: JwtService){}
 
-  async signIn(username: string, pass: string) {
-    const user = await this.userService.findOne(username);
-    if (user?.password !== pass) {
-      throw new UnauthorizedException();
+    login(user: User): UserToken {
+        //Transforma o user em um JWT
+        const payload: UserPayload = {
+            sub: user.id,
+            email: user.email,
+            name: user.name,
+        }
+
+        const jwtToken = this.jwtService.sign(payload);
+
+        return {
+            access_token: jwtToken
+        };
     }
-    const payload = { sub: user.userId, username: user.username };
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
-  }
+
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findOneByEmail(email);
+
+        if (user) {
+           //checar se a senha que foi informada correspooinde a hash que esta no banco
+
+           const isPasswaordValid = await bcrypt.compare(password, user.password);
+
+           if (isPasswaordValid) {
+            return{
+                ...user,
+                password: undefined,
+            };
+           }
+        }
+            //se chegar aqui, significa que náo encontrou um User e/ou a senha náo corresponde
+            throw new Error("Email addres or password provided is incorrect.");
+
+    }
 }
