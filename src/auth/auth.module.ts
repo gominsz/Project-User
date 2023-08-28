0af508ca1d38cd99dd151c12058bb4dd206ccbb4
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-28 17:48:32 -0300

    implements (JWT_SECRET, expiresIn(Day), JwtStrategy, LoginValidationMiddleware

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LoginValidationMiddleware } from './middleares/login-validation.middleware';
 
 @Module({
-  imports: [JwtModule.register({ secret: 'hard!to-guess_secret' })],
+  imports: [UserModule, JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions:{expiresIn: '30d'}
+  })],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy, JwtStrategy]
 })
-export class AuthModule {}
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('login');
+  }
+}
