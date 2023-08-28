55e2766d7c94a4dcb90e105d57742ddef6e62f1b
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-28 17:43:30 -0300

    APP_GUARD (jwt-auth.guard) end JwtAuthGuard

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module'; 
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService,],
+  providers: [AppService, {provide: APP_GUARD, useClass: JwtAuthGuard,}],
 })
 export class AppModule {}
