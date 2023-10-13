ade3abbd9f5ef04fb7394fcd1b3ce6612d17f15b
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-10-13 14:21:06 -0300

    feat(genderse): atribuição de funçoes as rotas usando (prismaService)

---
+++ src/genderse/genderse.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { CreateGenderseDto } from './dto/create-genderse.dto';
+import { UpdateGenderseDto } from './dto/update-genderse.dto';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class GenderseService {
+  constructor(private prismaService: PrismaClient) {}
+  async create(createGenderseDto: CreateGenderseDto) {
+    return this.prismaService.genres.create({
+      data: {
+        name: createGenderseDto.name,
+      },
+    });
+  }
+
+  findAll() {
+    return this.prismaService.genres.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prismaService.genres.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: string, updateGenderseInput: UpdateGenderseDto) {
+    return this.prismaService.genres.update({
+      where: { id },
+      data: updateGenderseInput,
+    });
+  }
+
+  async remove(id: string) {
+    return this.prismaService.genres.delete({
+      where: { id },
+    });
+  }
+}
