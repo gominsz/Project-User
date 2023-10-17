4f5c91dd23fe75f308bfba20721c4757159b57a5
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-10-17 13:13:26 -0300

    feat(user): constructor user para tests das rotas

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,13 @@
 export class User {
-    id?: string;
-    email: string;
-    password: string;
-    name: string;
+  id?: string;
+  email: string;
+  password: string;
+  name: string;
+
+  constructor(user?: Partial<User>) {
+    this.id = user.id;
+    this.email = user.email;
+    this.password = user.password;
+    this.name = user.name;
   }
+}
