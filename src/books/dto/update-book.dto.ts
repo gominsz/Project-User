15a0afabdd12039d429bec0f0e8cff6f698c194b
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-10-17 13:05:46 -0300

    feat(book): adicionando forma de update (geners_id)

--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,4 +1,29 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookDto } from './create-book.dto';
+import { IsDateString, IsOptional, IsString } from 'class-validator';
 
-export type UpdateBookInput = Partial<CreateBookDto>;
+export class UpdateBookInput {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  authors?: string;
+
+  @IsDateString()
+  @IsOptional()
+  publication_date?: Date;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  publishing_company?: string;
+
+  @IsString()
+  @IsOptional()
+  geners_id?: string;
+}
